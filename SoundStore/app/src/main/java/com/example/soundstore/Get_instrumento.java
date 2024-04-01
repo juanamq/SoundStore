1f8ea008558d3fca26963cb54bae10092d407f04
@juanamq <juanaquiceno5@gmail.com>
2024-03-31 22:01:32 -0500

    aplicacion-web

---
+++ SoundStore/app/src/main/java/com/example/soundstore/Get_instrumento.java
@@ -0,0 +1,95 @@
+package com.example.soundstore;
+
+public class Get_instrumento {
+
+    int id_instrumento;
+    String nombre;
+    String marca;
+    String stock;
+    String foto;
+    String tipo;
+    String color;
+    String modelo;
+    String precio;
+    String fecha_registro;
+
+    public int getId_instrumento() {
+        return id_instrumento;
+    }
+
+    public void setId_instrumento(int id_instrumento) {
+        this.id_instrumento = id_instrumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getFecha_registro() {
+        return fecha_registro;
+    }
+
+    public void setFecha_registro(String fecha_registro) {
+        this.fecha_registro = fecha_registro;
+    }
+}
