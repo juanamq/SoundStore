f096f46a63cb388a66647df4e994ad51711b9990
@juanamq <juanaquiceno5@gmail.com>
2024-03-31 22:15:24 -0500

    aplicacion-web

---
+++ Soundstore/application/views/admin/historialVentas.php
@@ -0,0 +1,43 @@
+<?php 
+    $dataHeader['titulo'] = "Historial Ventas";
+    $dataHead['encabezado'] = "HISTORIAL DE VENTAS";
+    $this->load->view('layouts/header', $dataHead, $dataHeader); 
+?>
+
+<div >
+    <table class="table_ventas">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Marca</th>
+                <th scope="col">Id instrumento</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio_unitario</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Precio total</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($ventas as $venta) : ?>
+                <tr>
+                    <td><?php echo $venta->id_historial_compras ?></td>
+                    <td><?php echo $venta->marca ?></td>
+                    <td><?php echo $venta->id_instrumento ?></td>
+                    <td><?php echo $venta->nombre ?></td>
+                    <td><?php echo $venta->precio_unitario ?></td>
+                    <td><?php echo $venta->cantidad ?></td>
+                    <td><?php echo $venta->precio_total ?></td>
+                    <td>
+                        <a class="boton_editar_user" href="guardar_per/<?php echo $venta->id_historial_compras ?>">Editar</a>
+                        <a class="boton_eliminar_user" href="borrar_per/<?php echo $venta->id_historial_compras ?>">Borrar</a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</div>
+
+<?php 
+    $this->load->view('layouts/footer'); 
+?>
